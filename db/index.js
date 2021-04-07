0fa84ebc00dfc6d349019e98a266204a36173e8a
Nick Palmer <ylnickpalmer@gmail.com>
2021-04-07 00:07:26 -0700

    fixed spelling error

--- db/index.js
+++ db/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
 
-const db = mongoose.connect(process.env.MONGODO_URI || 'mongodb://localhost/workout', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+const db = mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
 
 module.exports = db
